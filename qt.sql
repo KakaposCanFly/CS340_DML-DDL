65693ccba06d44e86c676e8061a5e291c0445fb9
Cole <theodoco@oregonstate.edu>
2022-05-10 17:46:05 -0700

    added create and insert statements for customers

--- qt.sql
+++ qt.sql
@@ -1,2 +1,15 @@
 -- Project Step 4 -- DML & DDL
 
+CREATE TABLE `customers` (
+  `customer_ID` INT AUTO_INCREMENT UNIQUE NOT NULL,
+  `customer_first_name` varchar(255) NOT NULL,
+  `customer_last_name` varchar(255) NOT NULL,
+  `customer_email` varchar(255) NOT NULL,
+  `customer_phone_number` varchar(64) NOT NULL,
+  `customer_address` varchar(255) NOT NULL,
+  `customer_birthdate` varchar(64) NOT NULL
+);
+
+
+INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
+('1','George','Immler','immler@massivehats.net','5039475829','ohio','04/26/90');
