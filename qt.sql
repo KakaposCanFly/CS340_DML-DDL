1001c4fdbb765e7b2e7111a7637a2728f84f8e4e
anjumathew01 <65983244+anjumathew01@users.noreply.github.com>
2022-05-10 18:06:33 -0700

    Update qt.sql

--- qt.sql
+++ qt.sql
@@ -19,3 +19,13 @@ INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, cus
 ('3','Harold','Shwab','gigachad@hidethepainharold.gov','5039400029','ohio','1990/04/26');
 INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
 ('4','Rachael','Breats','breats@iliketoshop.net','5039475425','new jersey','1984/01/19');
+
+
+
+INSERT INTO distributors (distriutor_ID, distributor_name, distributor_address, distributor_email, distributor_phone, distributor_contact_person) VALUES 
+('1','Raf''s Drip','nebraska','sweetkicks@evangelizeddd.com','5039475829','Josh');
+INSERT INTO distributors (distriutor_ID, distributor_name, distributor_address, distributor_email, distributor_phone, distributor_contact_person) VALUES 
+('2','Mike''s Shirts','washington','	mikesshirts@quantity.net','	5039412329','Mike');
+INSERT INTO distributors (distriutor_ID, distributor_name, distributor_address, distributor_email, distributor_phone, distributor_contact_person) VALUES 
+('3','Bailey''s Rings','new york','ringmyphone@snazzy.com','5039400029','Rick');
+
