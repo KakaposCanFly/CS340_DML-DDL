1d53d54e19f2a378d4ac1b532af5204b129a1c4c
anjumathew01 <65983244+anjumathew01@users.noreply.github.com>
2022-05-10 17:51:51 -0700

    Update qt.sql

--- qt.sql
+++ qt.sql
@@ -7,9 +7,15 @@ CREATE TABLE `customers` (
   `customer_email` varchar(255) NOT NULL,
   `customer_phone_number` varchar(64) NOT NULL,
   `customer_address` varchar(255) NOT NULL,
-  `customer_birthdate` varchar(64) NOT NULL
+  `customer_birthdate` date NOT NULL
 );
 
 
 INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
 ('1','George','Immler','immler@massivehats.net','5039475829','ohio','04/26/90');
+INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
+('2','June','Elizabeth','	elizabeth@greenthumb.net','	5039412329','	portland','	02/30/87');
+INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
+('3','Harold','Shwab','gigachad@hidethepainharold.gov','5039400029','ohio','04/26/90');
+INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
+('4','Rachael','Breats','breats@iliketoshop.net','5039475425','new jersey','01/19/84');
