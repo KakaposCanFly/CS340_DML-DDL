363a89ce2285303d683f3ed784872e84847447e8
Cole <theodoco@oregonstate.edu>
2022-05-10 18:25:00 -0700

    combined insert statements

--- qt.sql
+++ qt.sql
@@ -39,38 +39,26 @@ CREATE TABLE `sale_orders` (
 )
 
 INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
-('1','George','Immler','immler@massivehats.net','5039475829','ohio','1990/04/26');
-INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
-('2','June','Elizabeth','elizabeth@greenthumb.net','5039412329','portland','1987/02/30');
-INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
-('3','Harold','Shwab','gigachad@hidethepainharold.gov','5039400029','ohio','1990/04/26');
-INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, customer_email, customer_phone_number, customer_address, customer_birthdate) VALUES 
-('4','Rachael','Breats','breats@iliketoshop.net','5039475425','new jersey','1984/01/19');
-
+    ('1','George','Immler','immler@massivehats.net','5039475829','ohio','1990/04/26'),
+    ('2','June','Elizabeth','elizabeth@greenthumb.net','5039412329','portland','1987/02/30'),
+    ('3','Harold','Shwab','gigachad@hidethepainharold.gov','5039400029','ohio','1990/04/26'),
+    ('4','Rachael','Breats','breats@iliketoshop.net','5039475425','new jersey','1984/01/19');
 
 
 INSERT INTO distributors (distriutor_ID, distributor_name, distributor_address, distributor_email, distributor_phone, distributor_contact_person) VALUES 
-('1','Raf''s Drip','nebraska','sweetkicks@evangelizeddd.com','5039475829','Josh');
-INSERT INTO distributors (distriutor_ID, distributor_name, distributor_address, distributor_email, distributor_phone, distributor_contact_person) VALUES 
-('2','Mike''s Shirts','washington','	mikesshirts@quantity.net','	5039412329','Mike');
-INSERT INTO distributors (distriutor_ID, distributor_name, distributor_address, distributor_email, distributor_phone, distributor_contact_person) VALUES 
-('3','Bailey''s Rings','new york','ringmyphone@snazzy.com','5039400029','Rick');
+    ('1','Raf''s Drip','nebraska','sweetkicks@evangelizeddd.com','5039475829','Josh'),
+    ('2','Mike''s Shirts','washington','	mikesshirts@quantity.net','	5039412329','Mike'),
+    ('3','Bailey''s Rings','new york','ringmyphone@snazzy.com','5039400029','Rick');
 
 INSERT INTO products (product_ID,product_type,product_name,distributor_ID,retail_price,release_date,quant_in_stock) VALUES 
-('1','shoes','Raf''s Godly Kicks','02496','419.99','02/30/22','1');
-INSERT INTO products (product_ID,product_type,product_name,distributor_ID,retail_price,release_date,quant_in_stock) VALUES 
-('2','t-shirt','Standard Long Sleeve','02092','	11.99','02/30/20','119');
-INSERT INTO products (product_ID,product_type,product_name,distributor_ID,retail_price,release_date,quant_in_stock) VALUES 
-('3','hat','Magician''s Tophat','04446','49.99','02/12/19','7');
-INSERT INTO products (product_ID,product_type,product_name,distributor_ID,retail_price,release_date,quant_in_stock) VALUES 
-
-('4','poncho','Unparalleled Comfort','	03002','79.99','07/08/21','22');
+    ('1','shoes','Raf''s Godly Kicks','02496','419.99','02/30/22','1'),
+    ('2','t-shirt','Standard Long Sleeve','02092','	11.99','02/30/20','119'),
+    ('3','hat','Magician''s Tophat','04446','49.99','02/12/19','7'),
+    ('4','poncho','Unparalleled Comfort','	03002','79.99','07/08/21','22');
 
 INSERT INTO sale_orders(order_number, customer_ID,cc_number, cc_exp_date,delivery_status,paid_status) VALUES 
-('1','12','12982918478729781', '05/24','delivered','paid');
-INSERT INTO sale_orders(order_number, customer_ID,cc_number, cc_exp_date,delivery_status,paid_status) VALUES 
-('2','13','12688918478829784','10/29','	delivered','unpaid');
-INSERT INTO sale_orders(order_number, customer_ID,cc_number, cc_exp_date,delivery_status,paid_status) VALUES 
-('3','14','75688912478829883','01/26','not delivered','paid');
+    ('1','12','12982918478729781', '05/24','delivered','paid'),
+    ('2','13','12688918478829784','10/29','	delivered','unpaid'),
+    ('3','14','75688912478829883','01/26','not delivered','paid');
 
 
