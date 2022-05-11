34747ebc400a10893d58fc968497e7c32b06febb
anjumathew01 <65983244+anjumathew01@users.noreply.github.com>
2022-05-10 18:12:49 -0700

    Update qt.sql

--- qt.sql
+++ qt.sql
@@ -29,3 +29,12 @@ INSERT INTO distributors (distriutor_ID, distributor_name, distributor_address, 
 INSERT INTO distributors (distriutor_ID, distributor_name, distributor_address, distributor_email, distributor_phone, distributor_contact_person) VALUES 
 ('3','Bailey''s Rings','new york','ringmyphone@snazzy.com','5039400029','Rick');
 
+INSERT INTO products (product_ID,product_type,product_name,distributor_ID,retail_price,release_date,quant_in_stock) VALUES 
+('1','shoes','Raf''s Godly Kicks','02496','419.99','02/30/22','1');
+INSERT INTO products (product_ID,product_type,product_name,distributor_ID,retail_price,release_date,quant_in_stock) VALUES 
+('2','t-shirt','Standard Long Sleeve','02092','	11.99','02/30/20','119');
+INSERT INTO products (product_ID,product_type,product_name,distributor_ID,retail_price,release_date,quant_in_stock) VALUES 
+('3','hat','Magician''s Tophat','04446','49.99','02/12/19','7');
+INSERT INTO products (product_ID,product_type,product_name,distributor_ID,retail_price,release_date,quant_in_stock) VALUES 
+('4','poncho','Unparalleled Comfort','	03002','	79.99','07/08/21','22');
+
