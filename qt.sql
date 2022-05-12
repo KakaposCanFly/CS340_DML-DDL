27771e3ab668909491c5482510fb303fed596203
Cole <theodoco@oregonstate.edu>
2022-05-12 14:54:39 -0700

    removed unnecessary comment

--- qt.sql
+++ qt.sql
@@ -1,5 +1,6 @@
 -- Project Step 4 -- DML & DDL
 
+DROP TABLE IF EXISTS `customers`;
 CREATE TABLE `customers` (
     `customer_ID` INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
     `customer_first_name` varchar(255) NOT NULL,
@@ -10,6 +11,7 @@ CREATE TABLE `customers` (
     `customer_birthdate` date NOT NULL
 );
 
+DROP TABLE IF EXISTS `distributors`;
 CREATE TABLE `distributors` (
     `distributor_ID` INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
     `distributor_name` varchar(255) NOT NULL,
@@ -19,16 +21,18 @@ CREATE TABLE `distributors` (
     `distributor_contact_person` varchar(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
-    `product_ID` INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    `product_ID` int AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
     `product_type` varchar(255) NOT NULL,
     `product_name` varchar(255) NOT NULL,
-    `distributor_ID` INT(16) NOT NULL,
-    `retail_price` INT(16) NOT NULL,
+    `distributor_ID` int(16) NOT NULL,
+    `retail_price` decimal(16) NOT NULL,
     `release_date` date NOT NULL,
-    `quant_in_stock` INT(16), NOT NULL
+    `quant_in_stock` int(16) NOT NULL
 );
 
+DROP TABLE IF EXISTS `sale_orders`;
 CREATE TABLE `sale_orders` (
     `order_number` INT(16) AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
     `customer_ID` INT(16) NOT NULL,
@@ -38,6 +42,7 @@ CREATE TABLE `sale_orders` (
     `order_complete` boolean NOT NULL
 );
 
+DROP TABLE IF EXISTS `sale_order_product`;
 CREATE TABLE `sale_order_products` (
     `order_number` int(16) NOT NULL,
     `product_ID` int(16) NOT NULL,
@@ -45,9 +50,9 @@ CREATE TABLE `sale_order_products` (
     `selling_price` decimal(16,2),
     `shipped` boolean NOT NULL,
     `shipping_date` date NOT NULL,
-    FOREIGN KEY ('order_number') REFERENCES 'sale_orders' ('order_number'),
-    FOREIGN KEY ('product_ID') REFERENCES 'products' ('product_ID'),
-    PRIMARY KEY ('order_number','product_ID')
+    FOREIGN KEY (`order_number`) REFERENCES `sale_orders` (`order_number`),
+    FOREIGN KEY (`product_ID`) REFERENCES `products` (`product_ID`),
+    PRIMARY KEY (`order_number`,`product_ID`)
 );
 
 
@@ -58,25 +63,25 @@ INSERT INTO customers (customer_ID, customer_first_name, customer_last_name, cus
     ('4','Rachael','Breats','breats@iliketoshop.net','5039475425','new jersey','1984/01/19');
 
 
-INSERT INTO distributors (distriutor_ID, distributor_name, distributor_address, distributor_email, distributor_phone, distributor_contact_person) VALUES 
+INSERT INTO distributors (distributor_ID, distributor_name, distributor_address, distributor_email, distributor_phone, distributor_contact_person) VALUES 
     ('1','Raf''s Drip','nebraska','sweetkicks@evangelizeddd.com','5039475829','Josh'),
-    ('2','Mike''s Shirts','washington','	mikesshirts@quantity.net','	5039412329','Mike'),
+    ('2','Mike''s Shirts','washington','mikesshirts@quantity.net','5039412329','Mike'),
     ('3','Bailey''s Rings','new york','ringmyphone@snazzy.com','5039400029','Rick');
 
 INSERT INTO products (product_ID,product_type,product_name,distributor_ID,retail_price,release_date,quant_in_stock) VALUES 
-    ('1','shoes','Raf''s Godly Kicks','02496','419.99','02/30/22','1'),
-    ('2','t-shirt','Standard Long Sleeve','02092','	11.99','02/30/20','119'),
-    ('3','hat','Magician''s Tophat','04446','49.99','02/12/19','7'),
-    ('4','poncho','Unparalleled Comfort','	03002','79.99','07/08/21','22');
+    ('1','shoes','Raf''s Godly Kicks','02496','419.99','1990/04/26','1'),
+    ('2','t-shirt','Standard Long Sleeve','02092','	11.99','1990/04/26','119'),
+    ('3','hat','Magician''s Tophat','04446','49.99','1990/04/26','7'),
+    ('4','poncho','Unparalleled Comfort','	03002','79.99','1990/04/26','22');
 
-INSERT INTO sale_orders(order_number, customer_ID,cc_number, cc_exp_date,delivery_status,paid_status) VALUES 
-    ('1','12','12982918478729781', '05/24','delivered','paid'),
-    ('2','13','12688918478829784','10/29','	delivered','unpaid'),
-    ('3','14','75688912478829883','01/26','not delivered','paid');
+INSERT INTO sale_orders(order_number, customer_ID, order_date, cc_number, cc_exp_date, order_complete) VALUES 
+    ('1','12','1984/01/19','12982918478729781','1990/04/15','1'),
+    ('2','13','1984/01/19','12688918478829784','1990/04/15','1'),
+    ('3','14','1984/01/19','75688912478829883','1990/04/15','0');
 
-INSERT INTO sale_order_products(order_number,product_ID,quantity,selling_price,shipping_status,shipping_date) VALUES 
-     ('2343','23423423','900','$43','shipped','04/23/21'),
-     ('2344','23434344','1045','$133','not shipped','03/21/19'),
-     ('2345','234234243','2000','$25','shipped','01/20/17');
+INSERT INTO sale_order_products(order_number,product_ID,quantity,selling_price,shipped,shipping_date) VALUES 
+     ('1','1','900','419.99','1','1984/01/19'),
+     ('2','2','1045','11.99','0','1984/01/19'),
+     ('3','3','2000','49.99','1','1984/01/19');
 
 
